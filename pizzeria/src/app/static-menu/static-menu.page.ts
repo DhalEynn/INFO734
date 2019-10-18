dd0072f02b7918507b5bfc4d925a990ff3eb2795
DahlEynn <DahlEynn@users.noreply.github.com>
2019-10-18 17:23:10 +0200

    Pizza type separation

--- pizzeria/src/app/static-menu/static-menu.page.ts
+++ pizzeria/src/app/static-menu/static-menu.page.ts
@@ -7,35 +7,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StaticMenuPage implements OnInit {
   private selectedItem: any;
-  private pizza: [string, string] = [
+  private pizzaTomate: [string, string] = [
     ['PINOZETANT', '9.00€'],
     ['MERQUET', '9.40€'],
     ['ROME', '9.40€'],
     ['PATELLIÈRE', '9.60€'],
-    ['CHANTABEAU', '9.60€'],
+    ['CHANTABEAU', '9.60€']
+  ];
+  private pizzaCreme: [string, string] = [
     ['CACTUS', '10.90€'],
     ['NÈVE', '10.60€'],
     ['VERCHÈRE', '10.60€']
   ];
-  private icons = [
-    'flask',
-    'wifi',
-    'beer',
-    'football',
-    'basketball',
-    'paper-plane',
-    'american-football',
-    'boat',
-    'bluetooth',
-    'build'
-  ];
-  public items: Array<{ title: string; note: string; icon: string }> = [];
+  public baseTomate: Array<{ title: string; note: string; icon: string }> = [];
+  public baseCreme: Array<{ title: string; note: string; icon: string }> = [];
+
   constructor() {
-    for (let i = 0; i < this.pizza.length; i++) {
-      this.items.push({
-        title: this.pizza[i][0],
-        note: 'Price : ' + this.pizza[i][1],
-        icon: 'pizza' //this.icons[Math.floor(Math.random() * this.icons.length)]
+    for (let i = 0; i < this.pizzaTomate.length; i++) {
+      this.baseTomate.push({
+        title: this.pizzaTomate[i][0],
+        note: 'Price : ' + this.pizzaTomate[i][1],
+        icon: 'pizza'
+      });
+    }
+    for (let i = 0; i < this.pizzaCreme.length; i++) {
+      this.baseCreme.push({
+        title: this.pizzaCreme[i][0],
+        note: 'Price : ' + this.pizzaCreme[i][1],
+        icon: 'pizza'
       });
     }
   }
