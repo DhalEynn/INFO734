91fd7b14532dc5f332bff83a981e8dedccfe75bb
DahlEynn <DahlEynn@users.noreply.github.com>
2020-01-15 10:39:03 +0100

    Adding the ingredient list in the card

--- pizzeria/src/app/static-menu/static-menu.page.ts
+++ pizzeria/src/app/static-menu/static-menu.page.ts
@@ -9,8 +9,9 @@ export class StaticMenuPage implements OnInit {
   private selectedItem: any;
   private pizzaTomate: any;
   private pizzaCreme: any;
-  public baseTomate: Array<{ name: string; price: string; icon: string }> = [];
-  public baseCreme: Array<{ name: string; price: string; icon: string }> = [];
+  private ingred: any;
+  public baseTomate: Array<{ name: string; price: string; ingredients: Array<string>; icon: string }> = [];
+  public baseCreme: Array<{ name: string; price: string; ingredients: Array<string>; icon: string }> = [];
 
   constructor() {
     this.pizzaTomate = [
@@ -25,10 +26,17 @@ export class StaticMenuPage implements OnInit {
       ['NÈVE', '10.60€'],
       ['VERCHÈRE', '10.60€']
     ];
+    this.ingred = ["Fromage", "Tomate", "Creme", "Oeuf", "Patate", "Cactus"];
     for (let i = 0; i < this.pizzaTomate.length; i++) {
+      /*var ing = "<ul>";
+      for (var x in this.ingred) {
+        ing = ing + "<li>" + this.ingred[x] + "</li>"
+      }
+      ing = ing + "</ul>"*/
       this.baseTomate.push({
         name: this.pizzaTomate[i][0],
         price: this.pizzaTomate[i][1],
+        ingredients: this.ingred, //ing
         icon: 'pizza'
       });
     }
@@ -36,6 +44,7 @@ export class StaticMenuPage implements OnInit {
       this.baseCreme.push({
         name: this.pizzaCreme[i][0],
         price: this.pizzaCreme[i][1],
+        ingredients: this.ingred,
         icon: 'pizza'
       });
     }
